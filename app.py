4feb090d0ac6b11e3407105de69c8201573f31db
Michael E. Fortunato <mef231@gmail.com>
2018-05-27 12:57:49 -0400

    ready for first deployment

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
+import json
+import requests
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from bokeh.plotting import figure, output_file, show, output_notebook
+from bokeh.embed import components
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+#url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker={ticker}&date.gte={start_date}&date.lte={end_date}&api_key=PyewNKgseooV1caGxUtG'
+url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker={ticker}&api_key=PyewNKgseooV1caGxUtG'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    div = ''
+    script = ''
+    ticker = ''
+    if request.method == 'POST':
+        today = datetime.today()
+        ticker = request.form.get('ticker')
+        start_date = request.form.get('start-date', today.strftime('%Y-%m-%d'))
+        end_date = request.form.get('end-date', (today - timedelta(days=30)).strftime('%Y-%m-%d'))
+        resp = requests.get(url.format(ticker=ticker, start_date=start_date, end_date=end_date))
+        res = json.loads(resp.text)['datatable']
+        df = pd.DataFrame(columns=[c['name'] for c in res['columns']], data=res['data']).drop(columns='ticker')
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.set_index('date')
+        x = df.index.values
+        y = df['close']
+        p = figure(x_axis_label='Date', y_axis_label='Closing Price', x_axis_type="datetime")
+        p.line(x, y, line_width=2)
+        script, div = components(p)
+    return render_template('index.html', script=script, div=div, ticker=ticker)
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
