03e318c7f880843e9d323e80c1bd0752d77f229a
Michael E. Fortunato <mef231@gmail.com>
2018-05-29 21:33:11 -0400

    bootstrap styles, logo and news

--- app.py
+++ app.py
@@ -9,56 +9,72 @@ from bokeh.embed import components
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-#url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker={ticker}&date.gte={start_date}&date.lte={end_date}&api_key=PyewNKgseooV1caGxUtG'
-url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker={ticker}&api_key=PyewNKgseooV1caGxUtG'
+
+chart_url = 'https://api.iextrading.com/1.0/stock/{ticker}/chart/5y'
+company_url = 'https://api.iextrading.com/1.0/stock/{ticker}/company'
+logo_url = 'https://api.iextrading.com/1.0/stock/{ticker}/logo'
+news_url = 'https://api.iextrading.com/1.0/stock/{ticker}/news/last/5'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     div = ''
     script = ''
     ticker = ''
+    company = ''
+    logo = ''
+    news = []
     if request.method == 'POST':
         today = datetime.today()
         ticker = request.form.get('ticker')
         start_date = request.form.get('start-date', today.strftime('%Y-%m-%d'))
         end_date = request.form.get('end-date', (today - timedelta(days=30)).strftime('%Y-%m-%d'))
-        resp = requests.get(url.format(ticker=ticker, start_date=start_date, end_date=end_date))
-        res = json.loads(resp.text)['datatable']
-        df = pd.DataFrame(columns=[c['name'] for c in res['columns']], data=res['data']).drop(columns='ticker')
-        df['date'] = pd.to_datetime(df['date'])
-        df = df.set_index('date')
-        x = df.index.values.astype(str)
-        y = df['close']
-        y_high = df['high']
-        div = plotly.offline.plot(
-            {
-                'data': [
-                    {'x': x, 'y': y, 'name': 'close'},
-                ],
-                'layout': {
-                    'title': ticker,
-                    'xaxis': {
-                        'title': 'Date',
-                        'showgrid': False,
-                        'showline': True,
-                        'linecolor': 'black',
-                        'linewidth': 2,
-                        'mirror': True
-                    },
-                    'yaxis': {
-                        'title': 'Closing Price',
-                        'showgrid': False,
-                        'showline': True,
-                        'linecolor': 'black',
-                        'linewidth': 2,
-                        'mirror': True
-                    },
-                }
-            }, output_type='div', include_plotlyjs=False)
-        #p = figure(x_axis_label='Date', y_axis_label='Closing Price', x_axis_type="datetime")
-        #p.line(x, y, line_width=2)
-        #script, div = components(p)
-    return render_template('index.html', script=script, div=div, ticker=ticker)
+        resp = requests.get(chart_url.format(ticker=ticker))
+        if resp.status_code == 200:
+          df = pd.DataFrame(data=json.loads(resp.text))
+          df['date'] = pd.to_datetime(df['date'])
+          df = df.set_index('date')
+          x = df.index.values.astype(str)
+          y = df['close']
+          div = plotly.offline.plot(
+              {
+                  'data': [
+                      {'x': x, 'y': y, 'name': 'close'},
+                  ],
+                  'layout': {
+                      'margin': {
+                        'l': 100,
+                        'r': 100,
+                        't': 10,
+                        'b': 100,
+                        'pad': 4
+                      },
+                      'xaxis': {
+                          'title': 'Date',
+                          'showgrid': False,
+                          'showline': True,
+                          'linecolor': 'black',
+                          'linewidth': 2,
+                          'mirror': True
+                      },
+                      'yaxis': {
+                          'title': 'Closing Price',
+                          'showgrid': False,
+                          'showline': True,
+                          'linecolor': 'black',
+                          'linewidth': 2,
+                          'mirror': True
+                      },
+                  }
+              }, output_type='div', include_plotlyjs=False, config={'showLink': False})
+          resp = requests.get(company_url.format(ticker=ticker))
+          company = json.loads(resp.text).get('companyName', '')
+          resp = requests.get(logo_url.format(ticker=ticker))
+          logo = json.loads(resp.text).get('url', '')
+          resp = requests.get(news_url.format(ticker=ticker))
+          news = json.loads(resp.text)
+        else:
+          company = 'Error: Cannot query ticker symbol: {ticker}'.format(ticker=ticker)
+    return render_template('index.html', div=div, ticker=ticker, company=company, logo=logo, news=news)
 
 @app.route('/about')
 def about():
